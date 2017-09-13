a6286d6157e3dd92af2b3f92f26be8ccc8bad892
leafspace <18852923073@163.com>
2017-09-13 18:52:47 +0800

    修改了部署内容 Signed-off-by: leafspace <18852923073@163.com>

--- 程序代码/loadPlayer.py
+++ 程序代码/loadPlayer.py
@@ -3,30 +3,38 @@ import pygame
 import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(1, GPIO.IN)
-GPIO.setup(2, GPIO.IN)
-GPIO.setup(3, GPIO.IN)
-GPIO.setup(4, GPIO.IN)
+GPIO.setup(1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 print("Begining to listen ...")
-
+pygame.mixer.init()
 while True:
-    if(GPIO.input(1) == 1):
-        print("播放音乐:do")
-        track1 = pygame.mixer.music.load(".mp3")
+    if(GPIO.input(1) == True):
+        print("play:do")
+        track1 = pygame.mixer.music.load("a1.mp3")
         pygame.mixer.music.play()
-    if(GPIO.input(2) == 1):
-        print("播放音乐:re")
-        track1 = pygame.mixer.music.load(".mp3")
+        time.sleep(5)
+        continue
+    if(GPIO.input(2) == True):
+        print("play:re")
+        track1 = pygame.mixer.music.load("a3.mp3")
         pygame.mixer.music.play()
-    if(GPIO.input(3) == 1):
-        print("播放音乐:mi")
-        track1 = pygame.mixer.music.load(".mp3")
+        time.sleep(5)
+        continue
+    if(GPIO.input(3) == True):
+        print("play:mi")
+        track1 = pygame.mixer.music.load("a4.mp3")
         pygame.mixer.music.play()
-    if(GPIO.input(4) == 1):
-        print("播放音乐:fa")
-        track1 = pygame.mixer.music.load(".mp3")
+        time.sleep(5)
+        continue
+    if(GPIO.input(4) == True):
+        print("play:fa")
+        track1 = pygame.mixer.music.load("a5.mp3")
         pygame.mixer.music.play()
+        time.sleep(5)
+        continue
 
 print("programe exit .")
 GPIO.cleanup()
